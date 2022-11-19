331672a47024a825d6e4f12be6506402685bad4b
Funt <funt_88@mail.ru>
2022-11-19 22:02:38 +0300

    completed task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+while (true)
+{
+    Console.Write("Введите натуральное число (N): ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int a = 1;
+    if (number <= 0)
+    {
+        Console.WriteLine("Ошибка! Введено НЕ натуральное число (N), попробуйте ещё раз.");
+        continue;
+    }
+    while (a <= number)
+    {
+        Console.Write(a * a * a++ + " ");
+    }
+    break;
+}
