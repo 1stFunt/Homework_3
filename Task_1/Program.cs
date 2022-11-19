23d62dc70897df0cec70069db4dc4bd021e4a8cb
Funt <funt_88@mail.ru>
2022-11-19 20:58:11 +0300

    completed task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+while (true)
+{
+    Console.Write("Введите пятизначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int mir1 = number; int mir2 = number;
+    if (number < 0)
+    {
+        number *= -1;
+    }
+    if (number < 100000 && number > 9999)
+    {
+        {
+            mir1 = number / 1000 % 10 + number / 10000 * 10;
+            mir2 = number % 10 * 10 + number / 10 % 10;
+        }
+        if (mir1 == mir2)
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Число НЕ является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Не верное число, введите пятизначно!");
+        continue;
+    }
+    break;
+}
